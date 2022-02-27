d4974aede22b4eedd88b1f19747d8804d625bbf7
elgganor <mohamed.rouabehi.code@gmail.com>
2022-02-27 10:07:33 +0100

    Refactoring isUndefined tests

--- test/unit/undefined.spec.js
+++ test/unit/undefined.spec.js
@@ -1,20 +1,63 @@
 const { expect } = require('chai');
 const isUndefined = require('../../src/undefined');
 
+const arrowFn = () => {
+    return 'arrowFn'
+}
+
+function basicFn () {
+    return 'basicFn';
+}
+
 describe('isUndefined', () => {
-    it('should return true when value is undefined type', () => {
-        expect(isUndefined()).to.be.true;
-        expect(isUndefined(undefined)).to.be.true;
-    });
+    describe('should return true', () => {
+        it('when value is empty', () => {
+            expect(isUndefined()).to.be.true;
+        });
 
-    it('should return false when value is not undefined type', () => {
-        expect(isUndefined('')).to.be.false;
-        expect(isUndefined('hello')).to.be.false;
+        it('when value is undefined', () => {
+            expect(isUndefined(undefined)).to.be.true;
+        });
+    })
 
-        expect(isUndefined(0)).to.be.false;
-        expect(isUndefined(2342)).to.be.false;
+    describe('should return false', () => {
+        it('when value is null', () => {
+            expect(isUndefined(null)).to.be.false;
+        });
 
-        expect(isUndefined({})).to.be.false;
-        expect(isUndefined({ foo: true })).to.be.false;
-    });
+        it('when value is a boolean', () => {
+            expect(isUndefined(true)).to.be.false;
+            expect(isUndefined(false)).to.be.false;
+        })
+
+        it('when value is a number', () => {
+            expect(isUndefined(0)).to.be.false;
+            expect(isUndefined(2342)).to.be.false;
+        })
+
+        it('when value is string', () => {
+            expect(isUndefined('')).to.be.false;
+            expect(isUndefined('hello')).to.be.false;
+        })
+
+        it('when value is an object', () => {
+            expect(isUndefined({})).to.be.false;
+            expect(isUndefined({ foo: true })).to.be.false;
+        })
+
+        it('when value is an array', () => {
+            expect(isUndefined([])).to.be.false;
+            expect(isUndefined([1, 2, 3, 4])).to.be.false;
+        })
+
+        it('when value is a date', () => {
+            expect(isUndefined(new Date())).to.be.false;
+            expect(isUndefined(new Date('10/01/2022'))).to.be.false;
+        })
+
+        it('when value is a function', () => {
+            expect(isUndefined(basicFn)).to.be.false;
+            expect(isUndefined(arrowFn)).to.be.false;
+        })
+    })
 });
